ebbece442ef136349f8f199ec449859cff6941a3
yingQiangShen <1528787321@qq.com>
2016-03-13 21:48:53 +0800

    Initial Commit

---
+++ 银联支付1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  银联支付1
+//
+//  Created by syq on 16/3/13.
+//  Copyright © 2016年 lanou.syq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
